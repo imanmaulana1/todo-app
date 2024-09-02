dc8ab3f3f866635f8341ac5cf83dce31e9176ea9
Iman Maulana <maulanaiman902@gmail.com>
2024-09-02 16:04:40 +0700

    refactor: add PropTypes validation for AddToDo component

--- src/components/AddToDo/index.jsx
+++ src/components/AddToDo/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import PropTypes from 'prop-types';
 import styles from './AddToDo.module.css';
 
 function AddToDo({ addTodo }) {
@@ -72,5 +73,9 @@ function AddToDo({ addTodo }) {
     </section>
   );
 }
+
+AddToDo.propTypes = {
+  addTodo: PropTypes.func,
+};
 
 export default AddToDo;
